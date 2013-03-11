1e1c9979cf567abf691b00355e38c707d3cc5d5f
Sunjay Cauligi <sunjayc@cs.washington.edu>
2013-03-11 13:12:46 -0700

    Policy file is now an argument to RunTriceratops

--- src/byteinstrumentation/RunTriceratops.java
+++ src/byteinstrumentation/RunTriceratops.java
@@ -7,20 +7,11 @@ import java.io.IOException;
 public class RunTriceratops {
     public static void main(String args[]) throws IOException {
         // TODO: Validate these
-        File inFile = new File(args[0]);
-        File outFile = new File(args[1]);
+        FileReader policyFile = new FileReader(args[0]);
+        File inFile = new File(args[1]);
+        File outFile = new File(args[2]);
         
-        TriceratopsPolicy tripolicy = TriceratopsPolicy.readJson(new FileReader("res/AndroidMouseActivity.policy"));
-        
-        // List<Triceratops.Function> validateFunctions = new ArrayList<>();
-        // validateFunctions.add(new Triceratops.Function("Lmy/android/mouse/AndroidMouseActivity;", true, "leftClick", "VLandroid/view/View;"));
-        // validateFunctions.add(new Triceratops.Function("Lmy/android/mouse/AndroidMouseActivity;", true, "rightClick", "VLandroid/view/View;"));
-        
-        // List<Triceratops.Function> restrictedFunctions = new ArrayList<>();
-        // restrictedFunctions.add(new Triceratops.Function("Landroid/util/Log;", false, "i", "ILjava/lang/String;Ljava/lang/String;"));
-        
-        // List<Precondition> conditions = new ArrayList<>();
-        // conditions.add(new Precondition(restrictedFunctions, validateFunctions, "clickValidate"));
+        TriceratopsPolicy tripolicy = TriceratopsPolicy.readJson(policyFile);
         
         Triceratops.instrumentDex(inFile, outFile, tripolicy);
         System.out.println("Done!");
